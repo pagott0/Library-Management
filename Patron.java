e433c522809cfb525e00c780edbad251d65c8bc3
Enzo Pagotto <enzovaspagotto@gmail.com>
2024-05-31 16:06:59 -0300

    add: patron.java

---
+++ Patron.java
@@ -0,0 +1,11 @@
+public class Patron {
+    private String name;
+    private String contactInformation;
+
+    public Patron(String name, String contactInformation) {
+        this.name = name;
+        this.contactInformation = contactInformation;
+    }
+
+    // Getters and setters
+}
