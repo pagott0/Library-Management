d7e9c175c77b5365a58442d9b9369efdbad627b0
Enzo Pagotto <enzovaspagotto@gmail.com>
2024-05-31 16:04:44 -0300

    add: book class

---
+++ Book.java
@@ -0,0 +1,15 @@
+public class Book {
+    private String title;
+    private String author;
+    private String isbn;
+    private String category;
+
+    public Book(String title, String author, String isbn, String category) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+        this.category = category;
+    }
+
+    // Getters and setters
+}
